244f523755f25723344b106ac3ef0a4cd59bdb62
Squizzoo <dziwoszpatryk@gmail.com>
2019-11-12 12:22:30 +0100

    Db en links toevoegen

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+include "php/connectDB.php";
+include "php/functions.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php addLink(); ?>
     <title>Document</title>
 </head>
 <body>
