f7423a11f37b32b543dafa7a7530ef2a10f718c0
Ben <harderwijk02@gmail.com>
2019-11-14 13:35:38 +0100

    zoekbalk werkend

---
+++ masterZoek.php
@@ -0,0 +1,39 @@
+<?php
+
+$q = ($_GET["searchBar"]);
+
+$conn = mysqli_connect('localhost', 'root', '', 'wideworldimporters');
+
+if ($q == ($_GET['searchBar'] == ' ')){
+    header('Location: zoeklijst.php');
+}
+
+if ($q == ($_GET['searchBar'] == 'Zoeken..')){
+    header('Location: zoeklijst.php');
+}
+
+?>
+
+
+<?php
+if (!isset($q)){
+    echo ' ';
+} else {
+    $query = mysqli_query($conn, "SELECT * FROM stockitems WHERE StockItemName LIKE '%$q%' OR SearchDetails LIKE '%$q%' OR Tags LIKE '%$q%'");
+    $num_rows = mysqli_num_rows($query);
+    ?>
+    <p><strong><?php echo $num_rows; ?> </strong> Results for "<?php echo $q; ?>"</p>
+    <?php
+    while ($row = mysqli_fetch_array($query)) {
+
+        $title = $row['StockItemName'];
+        $details = $row['SearchDetails'];
+        $tags = $row['Tags'];
+        $image = $row['Photo'];
+
+        echo "<h3>" . $title . "</h3><p>" . $details . $image . $tags. "</p><br> " . ". <br>";
+
+
+    }
+}
+?>
---
+++ nav.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="masterStyle.css">
+    <title>inlog</title>
+</head>
+<body>
+<div id="menuBalk">
+    <div class="algMargin">
+        <img src="wwi_logo_wit.png" id="logo">
+        <div class="categ">Categorieën</div>
+        <div class = "zoekinput">
+            <form action="masterZoek.php" method="GET" id="searchBar">
+                <input type="text" onfocus="this.value= '' " name="searchBar" value="Zoeken.." maxlength="25" autocomplete="on" onmousedown="" onblur="">
+                <input type="submit" id="searchBtn" value="Zoeken!">
+            </form>
+        </div>
+        <img src="account.png" class="menubalk-foto" " onclick="document.getElementById('accountclick').style.display='block'">
+        <img src="winkelwagen.png" class = "menubalk-foto">
+    </div>
+</div>
+<div id="accountclick">
+    <div class="modal">
+        <form id="inlog" action="/index.php" method="post">
+            <span onclick="document.getElementById('accountclick').style.display='none'" class="close">&times;</span>
+            <input type="text" placeholder="Username" id="username" required><br>
+            <input type="password" placeholder="Password" id="userpw" required><br>
+            <input type="submit" placeholder="Verstuur" id="send"><br>
+        </form>
+    </div>
+</div>
+</body>
+</html>
+
+<script>
+    var modal = document.getElementById('accountclick');
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
