2a151ed3ae8fe91508ec702f223ed425ac89af38
Ben <harderwijk02@gmail.com>
2019-11-19 09:56:01 +0100

    nav.php & masterZoek.php aangepast zodat producten op categorie kunnen opgezocht worden

--- masterZoek.php
+++ masterZoek.php
@@ -1,33 +1,56 @@
 <?php
+function categ($conn){
+    if (isset($_GET['id'])){
+        return TRUE;
+    }
+}
+?>
+
+<?php
 include "nav.php";
 include_once "php\connectDB.php";
 
-$q = ($_GET["searchBar"]);
-
-if ($q == ($_GET['searchBar'] == ' ')){
-    header('Location: index.php');
+if(isset($_GET['id'])){
 }
-
+else{
+    $q = ($_GET["searchBar"]);
+    if ($q == ($_GET['searchBar'] == ' ')){
+        header('Location: index.php');
+    }
+}
 ?>
 
 
 <?php
-if (!isset($q)){
-    echo ' ';
-} else {
+if (categ($conn)=== TRUE){
+    $groupID = $_GET['id'];
+    $query = mysqli_query($conn, "SELECT * FROM stockitems S JOIN stockitemstockgroups SG ON SG.StockItemID = S.StockItemID WHERE SG.StockGroupID = '$groupID'");
+    $num_rows = mysqli_num_rows($query);
+}
+else {
     $query = mysqli_query($conn, "SELECT * FROM stockitems WHERE StockItemName LIKE '%$q%' OR SearchDetails LIKE '%$q%' OR Tags LIKE '%$q%'");
     $num_rows = mysqli_num_rows($query);
-    ?>
-    <p><strong><?php echo $num_rows; ?> </strong> Results for "<?php echo $q; ?>"</p>
-    <?php
-        while ($row = mysqli_fetch_array($query)) {
-
-            $title = $row['StockItemName'];
-            $details = $row['SearchDetails'];
-            $tags = $row['Tags'];
-            $image = $row['Photo'];
-
-            echo "<h3>" . $title . "</h3><p>" . $details . $image . $tags. "</p><br> " . ". <br>";
+}
+?>
+    <p><strong><?php echo $num_rows; ?> </strong> Results for "
+        <?php if(isset($q)){
+            echo ($q);
         }
+        elseif (isset($groupID)){
+            $query2 = mysqli_query($conn, "SELECT StockGroupName FROM stockgroups WHERE '$groupID' = StockGroupID");
+            $resultaat = mysqli_fetch_row($query2);
+            echo ($resultaat[0]);
+        }
+        ?>"</p>
+    <?php
+    while ($row = mysqli_fetch_array($query)) {
+
+        $title = $row['StockItemName'];
+        $details = $row['SearchDetails'];
+        $tags = $row['Tags'];
+        $image = $row['Photo'];
+
+        echo "<h3>" . $title . "</h3><p>" . $details . $image . $tags . "</p><br> " . ". <br>";
     }
-    ?>
+?>
+
