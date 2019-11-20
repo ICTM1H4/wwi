e61a5ac135636d9cab91af98af90ba09b750c36e
Squizzoo <dziwoszpatryk@gmail.com>
2019-11-20 10:07:11 +0100

    product pagina

--- structure/productpage.php
+++ structure/productpage.php
@@ -7,10 +7,10 @@ $data = getItem($conn, $id);
         <div class="mySlides fade">
             <div class="numbertext">1 / 1</div>
             <img src="Geen_foto_helaas_beschikbaar.png" alt="werkt niet">
-        <!-- <div class="text">Caption Text</div> -->
+        <div class="text">Caption Text</div>
         </div>
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        <!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a> -->
+        <!-- <a class="next" onclick="plusSlides(1)">&#10095;</a> -->
     </div>
     <div style="text-align:center">
         <span class="dot" onclick="currentSlide(1)"></span>
@@ -18,7 +18,7 @@ $data = getItem($conn, $id);
         <!-- <span class="dot" onclick="currentSlide(3)"></span> -->
     </div>
 </div>
-<div class="content">
+<div class="ng">
     <div class="naam">Naam: <?php echo $data["StockItemName"] ?></div>
     <div class="prijs md5">Prijs: <?php echo $data["RecommendedRetailPrice"] ?></div>
     <div class="link"><a class="md5" href="<?php echo $data['StockItemID'] ?>"> Voeg toe aan winkelmandje </a></div>
