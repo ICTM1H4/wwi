822de8e850d3552cf1027d4214118a7f86db5a73
skolm <skolmschate@hotmail.nl>
2019-11-28 18:55:22 +0100

    basis styling overzicht pagina

--- overzicht.php
+++ overzicht.php
@@ -9,28 +9,29 @@ include "php/functions.php";
     <title>Wide World Importers</title>
 </head>
 <body>
-<div class = 'progressBar'><?php echo progressBar()?></div>
-    <div class="algMargin">
+<div class="algMargin">
+<div class = 'progressBar'><?php echo "<h1>" . progressBar() . "</h1>"?></div>
         <form class="factuurData">
             <div class="klantInfo">
-                Naam:
-                <input type="text" name="voornaam" placeholder="Voornaam">
+                <h2>Uw gegevens</h2>
+                Naam <input type="text" name="voornaam" placeholder="Voornaam">
                 <input type="text" name="tussenvoegsel" placeholder="tussenvoegsels">
                 <input type="text" name="achternaam" placeholder="Achternaam" >
             </div>
             <div class="adres">
-                Postcode: <input type="text" name="postcode" placeholder="1234 AB">
-                Adres: <input type="text" name="straat" placeholder="Straat + straatnummer">
+                Postcode <input type="text" name="postcode" placeholder="1234 AB">
+                Adres <input type="text" name="straat" placeholder="Straat + straatnummer">
                 <input type="text" name="BestelToev" placeholder="Evt. toev.">
             </div>
-            <div class = "email">E-mail: <input type="text" name="BestelEmail" placeholder="E-mailadres">
-            Telefoonnummer: <input type="text" name="BestelTel" placeholder="Telefoonnummer">
+            <div class = "email">E-mail <input type="text" name="BestelEmail" placeholder="E-mailadres">
+            Telefoonnummer <input type="text" name="BestelTel" placeholder="Telefoonnummer">
             </div>
         </form>
-        <div class="VerzendType">
-            <p class="normLever"> Prijs: €<?php //$normLever ?></p>
-            <p class="ophalen"> Prijs: €<?php //$ophalen ?></p>
-            <p class="eenDagLever"> Prijs: €<?php //$eenDagLever ?></p>
+        <div class="verzendType">
+            <h1>Verzendprijzen</h1>
+            <p class="normLever"> Normale levering €<?php //$normLever ?></p>
+            <p class="ophalen"> Ophalen €<?php //$ophalen ?></p>
+            <p class="eenDagLever"> Express levering €<?php //$eenDagLever ?></p>
         </div>
     </div>
 </body>
