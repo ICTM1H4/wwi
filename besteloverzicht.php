7ac912f0f9f88c536bb186b4b62c95dabe6d627d
skolm <skolmschate@hotmail.nl>
2019-11-29 11:32:51 +0100

    begin aan besteloverzicht en aanpassingen met de style daarvan

--- besteloverzicht.php
+++ besteloverzicht.php
@@ -15,22 +15,32 @@ include "php/functions.php";
 
 <body>
 <div class="algMargin">
-    <div class = "overzichtAdres">
-        <div> <h2>Aflever- en factuuradres</h2></div>
-        <div class="overzichtKlantNaam">voornaam tussenvoegsel achternaam </div>
-        <div class="overzichtStraat"> Straat Straatnummer </div>
-        <div class="overzichtPostcode"> XXXX XX Plaats <br><br></div>
-    </div>
-    <div class="overzichtBezorging">
-        <div><h2>Bezorging</h2></div>
-        <div >Bezorgingswijze<br><br></div>
-        <div>... euro</div>
-    </div>
-    <div class = "betalingswijze">
-        <div><h2>Betaling</h2></div>
-        <div>
-
+    <div class = "overzichtKlant">
+        <div class = "overzichtAdres">
+            <div> <h2>Aflever- en factuuradres</h2></div>
+            <div class="overzichtKlantNaam">voornaam tussenvoegsel achternaam </div>
+            <div class="overzichtStraat"> Straat Straatnummer </div>
+            <div class="overzichtPostcode"> XXXX XX Plaats <br><br></div>
         </div>
+
+        <div class="overzichtBezorging">
+            <div><h2>Bezorging</h2></div>
+            <div>Bezorgingswijze<br><br></div>
+            <div>... euro</div>
+        </div>
+        <div class = "betalingswijze">
+            <div><h2>Betaling</h2></div>
+            <div>Ideal</div>
+        </div>
+    </div>
+    <div class = "overzichtBestelling">
+        <div><h2>Bestelling</h2></div>
+        <div>
+            <?php
+            for($i = 0; $i < 5; $i++){
+                echo "Product";
+            }
+            ?></div>
     </div>
 </div>
 </body>
