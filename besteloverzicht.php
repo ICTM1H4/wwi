4559ddfcabf638cc4fd6e4108518dd3f25059041
skolm <skolmschate@hotmail.nl>
2019-11-29 11:13:12 +0100

    begin aan besteloverzicht en aanpassingen met de style daarvan

--- besteloverzicht.php
+++ besteloverzicht.php
@@ -1,1 +1,37 @@
 <?php
+include "nav.php";
+include "php/functions.php";
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="navStyle.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Wide World Importers</title>
+</head>
+
+<head>
+    <title>Wide World Importers</title>
+</head>
+
+<body>
+<div class="algMargin">
+    <div class = "overzichtAdres">
+        <div> <h2>Aflever- en factuuradres</h2></div>
+        <div class="overzichtKlantNaam">voornaam tussenvoegsel achternaam </div>
+        <div class="overzichtStraat"> Straat Straatnummer </div>
+        <div class="overzichtPostcode"> XXXX XX Plaats <br><br></div>
+    </div>
+    <div class="overzichtBezorging">
+        <div><h2>Bezorging</h2></div>
+        <div >Bezorgingswijze<br><br></div>
+        <div>... euro</div>
+    </div>
+    <div class = "betalingswijze">
+        <div><h2>Betaling</h2></div>
+        <div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
