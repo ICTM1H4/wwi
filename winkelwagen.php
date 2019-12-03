2f2391137e1366285fab5e90c37f9fb7150a654f
joeyk <joeykoopal@hotmail.com>
2019-12-03 12:43:00 +0100

    totaalprijs is gucci

--- winkelwagen.php
+++ winkelwagen.php
@@ -23,13 +23,23 @@
     <?php
     $totaal = 0;
     $totaalprijs = 0;
+    $verzendkosten = 0;
+    $totaalartikelen = $totaalprijs*1.21+$verzendkosten;
+
+   if ($totaalprijs >= 50){
+       $verzendkosten = 0;
+
+   } else $verzendkosten = 6.95;
+
+
+$totaalprijs=0;
     foreach ($_SESSION['cart'] as $result) {
         echo "<br>";
-        $queryscproducts = mysqli_query($conn, "SELECT StockItemName FROM stockitems WHERE StockItemID = ".$result["product_id"]."");
+        $queryscproducts = mysqli_query($conn, "SELECT StockItemName FROM stockitems WHERE StockItemID = " . $result["product_id"] . "");
         $title = $queryscproducts->fetch_assoc();
-        echo  $title["StockItemName"];
+        echo $title["StockItemName"];
         echo "<br>";
-        $queryscproducts = mysqli_query($conn, "SELECT RecommendedRetailPrice FROM stockitems WHERE StockItemID = ".$result["product_id"]."");
+        $queryscproducts = mysqli_query($conn, "SELECT RecommendedRetailPrice FROM stockitems WHERE StockItemID = " . $result["product_id"] . "");
         $price = $queryscproducts->fetch_assoc();
         echo "€" . $price["RecommendedRetailPrice"];
 
@@ -37,23 +47,19 @@
         echo "<br>";
         ?>
 
-        <button type="submit" name="decrease" value="+" id="decrease"> - </button>
-        <button type="submit" name="increase" value="+" id="increase"> + </button>
-        <button type="submit" name="delete" value="x" id="delete"> Verwijderen </button>
+        <button type="submit" name="decrease" value="+" id="decrease"> -</button>
+        <button type="submit" name="increase" value="+" id="increase"> +</button>
+        <button type="submit" name="delete" value="x" id="delete"> Verwijderen</button>
+        <hr>
 
 
         <?php
 
-        $totaalprijs = $price['RecommendedRetailPrice'] += $price['RecommendedRetailPrice'] ;
-        echo "<br><hr>";
-        //echo "<br>" . "productID: " . $result['product_id']."<br>" . ;
-        //test
+
+
+        $totaalprijs = $totaalprijs + $price["RecommendedRetailPrice"];
+        $totaalartikelen = round($totaalprijs*1.21+$verzendkosten,2);
     }
-
-
-    //$totaalartikelen = array_sum($result['product_id']); // veranderen naar een POST om de prijzen te bepalen //
-    //$verzendkosten = 0;
-    //$prijs = array_count_values($price);
 
 
     ?>
@@ -64,6 +70,9 @@
 
     <form action="winkelwagen.php">
     <h3>Aantal artikelen: <?php echo $totaal ?> </h3><br>
+        <h3>Totaalprijs: <?php echo "€" . $totaalprijs ?> (exl btw) </h3>
+        <h3>Verzendkosten: <?php echo "€" . $verzendkosten ?>  </h3><hr>
+        <h3>Totaal: <?php echo "€" . $totaalartikelen ?> (inc btw)</h3>
 
         <input type="submit" value="Verder naar bestellen" id="verder">
     </form>
