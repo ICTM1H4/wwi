300574ba7fe900fd92c54157e03353a1a61190dd
skolm <skolmschate@hotmail.nl>
2019-12-04 11:30:40 +0100

    Mollie toegevoegd

--- betaling.php
+++ betaling.php
@@ -2,12 +2,29 @@
 include "php/functions.php";
 include "php/connectDB.php";
 include "altnav.php";
+
+/*
+ * Make sure to disable the display of errors in production code!
+ */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once __DIR__ . "/mollie/vendor/autoload.php";
+require_once __DIR__ . "/mollie/vendor/guzzlehttp/guzzle/src/functions.php";
+/*
+ * Initialize the Mollie API library with your API key.
+ *
+ * See: https://www.mollie.com/dashboard/developers/api-keys
+ */
+$mollie = new \Mollie\Api\MollieApiClient();
+$mollie->setApiKey("test_RBf2gpgfc9xfRvfMmTVFEjyN9wHbk7");
 ?>
 
 <html>
 <head>
     <link rel="stylesheet" href="css/style.css">
     <title>Wide World Importers</title>
+
 </head>
 
 <body>
