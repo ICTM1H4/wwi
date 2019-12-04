bb207eb65e04761e4ebf52f84fc20210e281a304
skolm <skolmschate@hotmail.nl>
2019-12-04 12:15:02 +0100

    mollie werkend

--- betaling.php
+++ betaling.php
@@ -1,8 +1,4 @@
 <?php
-include "php/functions.php";
-include "php/connectDB.php";
-include "altnav.php";
-
 /*
  * Make sure to disable the display of errors in production code!
  */
@@ -18,44 +14,79 @@ require_once __DIR__ . "/mollie/vendor/guzzlehttp/guzzle/src/functions.php";
  */
 $mollie = new \Mollie\Api\MollieApiClient();
 $mollie->setApiKey("test_RBf2gpgfc9xfRvfMmTVFEjyN9wHbk7");
+
+
+$order = $mollie->orders->create([
+    "amount" => [
+        "value" => "0.01",
+        "currency" => "EUR"
+    ],
+    "billingAddress" => [
+        "organizationName" => "Mollie B.V.",
+        "streetAndNumber" => "Keizersgracht 313",
+        "city" => "Amsterdam",
+        "region" => "Noord-Holland",
+        "postalCode" => "1234AB",
+        "country" => "NL",
+        "title" => "Dhr.",
+        "givenName" => "Piet",
+        "familyName" => "Mondriaan",
+        "email" => "piet@mondriaan.com",
+        "phone" => "+31309202070",
+    ],
+    "shippingAddress" => [
+        "organizationName" => "Mollie B.V.",
+        "streetAndNumber" => "Keizersgracht 313",
+        "streetAdditional" => "4th floor",
+        "city" => "Haarlem",
+        "region" => "Noord-Holland",
+        "postalCode" => "5678AB",
+        "country" => "NL",
+        "title" => "Mr.",
+        "givenName" => "Chuck",
+        "familyName" => "Norris",
+        "email" => "norris@chucknorrisfacts.net",
+    ],
+    "metadata" => [
+        "order_id" => "1337",
+        "description" => "Lego cars"
+    ],
+    "consumerDateOfBirth" => "1958-01-31",
+    "locale" => "nl_NL",
+    "orderNumber" => "1337",
+    "redirectUrl" => "http://localhost/wwi/bevestiging.php",
+    "webhookUrl" => "https://example.org/webhook",
+    "method" => "creditcard",
+    "lines" => [
+        [
+            "type" => "physical",
+            "sku" => "5702016116977",
+            "name" => "LEGO 42083 Bugatti Chiron",
+            "productUrl" => "https://shop.lego.com/nl-NL/Bugatti-Chiron-42083",
+            "imageUrl" => 'https://sh-s7-live-s.legocdn.com/is/image//LEGO/42083_alt1?$main$',
+            "metadata" => [
+                "order_id" => "1337",
+                "description" => "Bugatti Chiron"
+            ],
+            "quantity" => 1,
+            "vatRate" => "0",
+            "unitPrice" => [
+                "currency" => "EUR",
+                "value" => "0.01"
+            ],
+            "totalAmount" => [
+                "currency" => "EUR",
+                "value" => "0.01"
+            ],
+            "vatAmount" => [
+                "currency" => "EUR",
+                "value" => "0.00"
+            ]
+        ],
+    ]
+]);
+
+header("Location: " . $order->getCheckoutUrl(), true, 303);
+
+
 ?>
-
-<html>
-<head>
-    <link rel="stylesheet" href="css/style.css">
-    <title>Wide World Importers</title>
-
-</head>
-
-<body>
-<div class="algMargin">
-    <div class="betalingkeuze">
-        <div class = "overzicht-border">
-            <h2>Betaalmethode IDEAL</h2>
-            <div>
-                <h3>Kies uw bank</h3>
-            </div>
-            <div>
-                <select >
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
-            </div>
-            <form action="bevestiging.php">
-                <input class = button-afrekenen type="submit" value="Doorgaan">
-            </form>
-        </div>
-    </div>
-    <div>
-        <form action="besteloverzicht.php">
-            <div class = "vorigestap">
-                <input class ="button-afrekenen" type ="submit" value="Vorige stap">
-            </div>
-        </form>
-    </div>
-</div>
-
-</body>
-</html>
